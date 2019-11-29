c1be71985e04f3a6e8971c00c6d3a1b48059f3b0
nataliia-v <nataly.verbenskaya@gmail.com>
2019-11-29 17:44:25 +0200

    ABC-1: +test commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-
-
-
-
-
-
-    <div className="App">
-      HELLO
-    </div>
-
-  );
+  return <div className="App">HELLO</div;
 }
 
 export default App;
