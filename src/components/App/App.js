1180f4a36c0cb4a33b3c91ef33a96b4b5bf058fa
nataliia-v <nataly.verbenskaya@gmail.com>
2019-11-30 13:04:02 +0200

    ABC-8: + (Header) added Header component with styles * (App) updated, in component added header

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.scss';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
