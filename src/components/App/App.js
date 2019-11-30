80af220644f7ff01f06f67f1a348a3535fb2e151
Nataliia Verbenska <46761205+nataliia-v@users.noreply.github.com>
2019-11-30 13:47:45 +0200

    minor: task/ABC-9: created Footer component and added in App(#12)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './App.module.scss';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const internName = "Nataliia Varbenska";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Footer internName={internName}/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+function Footer({internName}) {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footerName}>{internName}</p>
+    </div>
+  );
+}
+
+export default Footer;
