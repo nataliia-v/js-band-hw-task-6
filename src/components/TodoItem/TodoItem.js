0a806d35b47c788d452222625d4b96d3e0af13ec
nataliia-v <nataly.verbenskaya@gmail.com>
2019-12-01 18:51:52 +0200

    ABC-13 + TodoItem added

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './TodoItem.css';
+
+export class TodoItem extends Component {
+
+  static click() {
+    console.log('clicked on buttons');
+  }
+
+  render() {
+
+    return (
+      <div className="todoItem">
+        <div className="iconDoneWrap"><i className="fa fa-check-square-o iconDone" /></div>
+
+        <p className="title">Title</p>
+        <p className="description">Description</p>
+        <div className='container'>
+          <span className='priority'>Priority</span>
+          <button className='dotsButton' type='button' onClick={() => TodoItem.click()}>...</button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Form } from '../Form/Form';
+import { TodoItem } from '../TodoItem/TodoItem';
 
 function Todos() {
   return (
     <div>
       <Form/>
+      <TodoItem/>
     </div>
   );
 }
