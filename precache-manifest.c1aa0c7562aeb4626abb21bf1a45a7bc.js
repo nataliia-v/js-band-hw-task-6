660a42ce03deaf8192bd1e0b257f029e8b8eb6d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 15:50:18 +0000

    Deploy nataliia-v/js-band-hw-task-6 to github.com/nataliia-v/js-band-hw-task-6.git:gh-pages

---
+++ precache-manifest.c1aa0c7562aeb4626abb21bf1a45a7bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5accefa9aa193c97b1b17f3fe47606",
+    "url": "/js-band-hw-task-6/index.html"
+  },
+  {
+    "revision": "34c7447d4633d58a6de2",
+    "url": "/js-band-hw-task-6/static/css/main.99ca1dd9.chunk.css"
+  },
+  {
+    "revision": "314d835edf72101a4208",
+    "url": "/js-band-hw-task-6/static/js/2.5cdd186f.chunk.js"
+  },
+  {
+    "revision": "34c7447d4633d58a6de2",
+    "url": "/js-band-hw-task-6/static/js/main.a080f0db.chunk.js"
+  },
+  {
+    "revision": "ba09b9f3d2cd2824722f",
+    "url": "/js-band-hw-task-6/static/js/runtime-main.de2a5d39.js"
+  }
+]);
